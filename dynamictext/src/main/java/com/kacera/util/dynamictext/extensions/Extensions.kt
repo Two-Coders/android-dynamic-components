e0436db60b0c9ed66f3cf489255bf0672d46c4ef
Tomáš Valenta <cz840311@gmail.com>
2020-04-08 08:38:00 +0200

    Add androidx.core:core dependency. Use HtmlCompat instead of the native Html.

--- dynamictext/src/main/java/com/kacera/util/dynamictext/extensions/Extensions.kt
+++ dynamictext/src/main/java/com/kacera/util/dynamictext/extensions/Extensions.kt
@@ -1,16 +1,9 @@
 package com.kacera.util.dynamictext.extensions
 
 import android.content.Context
-import android.os.Build
-import android.text.Html
 import androidx.annotation.StringRes
+import androidx.core.text.HtmlCompat
 
 @Suppress("DEPRECATION")
 fun Context.getText(@StringRes id: Int, vararg args: Any?): CharSequence =
-    with(String.format(getString(id), *args)) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
-        } else {
-            Html.fromHtml(this)
-        }
-    }
+    HtmlCompat.fromHtml(String.format(getString(id), *args), HtmlCompat.FROM_HTML_MODE_COMPACT)
